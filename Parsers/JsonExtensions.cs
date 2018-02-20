be6b6e8c5ccb291e20211460103e4bc50b412a96
Costin Morariu <cm@interactivemedica.com>
2018-02-20 22:15:53 +0100

    Added JsonParser extensions.

---
+++ Parsers/JsonExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Costin Morariu. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Newtonsoft.Json.Linq;
+
+namespace HttpApiClient.Parsers
+{
+    public static class JsonExtensions
+    {
+        /// <summary>
+        /// Determines if token is null or empty.
+        /// </summary>
+        /// <param name="token">Json.NET token instance.</param>
+        /// <returns>True if token is null or empty or doesn't have any value, otherwise false.</returns>
+        public static bool IsNullOrEmpty(this JToken token)
+        {
+            return (token == null) ||
+                   (token.Type == JTokenType.Array && !token.HasValues) ||
+                   (token.Type == JTokenType.Object && !token.HasValues) ||
+                   (token.Type == JTokenType.String && token.ToString() == string.Empty) ||
+                   (token.Type == JTokenType.Null);
+        }
+    }
+}
---
+++ Parsers/JsonParserExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Costin Morariu. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+namespace HttpApiClient.Parsers
+{
+    public static class JsonParserExtensions
+    {
+        /// <summary>
+        /// Deserializes Problemator response.
+        /// </summary>
+        /// <typeparam name="T">The type of the instance.</typeparam>
+        /// <param name="parser">Parser associated with the response.</param>
+        /// <returns>An instance or default(T) in case of an empty Json.</returns>
+        public static T To<T>(this JsonParser parser)
+        {
+            var jToken = parser.GetData();
+            if (jToken.IsNullOrEmpty())
+            {
+                return default(T);
+            }
+
+            return jToken.ToObject<T>();
+        }
+    }
+}
